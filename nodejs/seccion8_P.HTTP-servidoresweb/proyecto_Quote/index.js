dd937161f6429721c1518dc980199134b217dead
LTUYU <franciscotuyupadron@gmail.com>
2020-03-21 20:03:53 -0600

    modificación de Quotes

--- nodejs/seccion8_P.HTTP-servidoresweb/proyecto_Quote/index.js
+++ nodejs/seccion8_P.HTTP-servidoresweb/proyecto_Quote/index.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const server = express();
 const {PORT} = require("./config") //importamos la port del otro archivo
+const {HomeRoutes} = require("./routes"); //importamos el método
+
+//middleware
+server.use(express.static('./public')); //verlo de manera publica
+server.use(express.json());//tranformar las entradas POST EN JSON
+
+server.use('/', HomeRoutes);
 
 server.listen(PORT, ()=>{
     console.log(`Aplication running in PORT ${PORT}`);
