64432a8215b884a521bd021ca92f65f756f26999
Luis Tuyu <e17081482@itmerida.edu.mx>
2020-03-29 14:56:36 -0500

    v47 entender como funciona expres y los https methods

--- nodejs/seccion8_P.HTTP-servidoresweb/servidor/index.js
+++ nodejs/seccion8_P.HTTP-servidoresweb/servidor/index.js
@@ -1,7 +1,32 @@
 const express = require("express");
 const server = express(); //nos devuelve un objeto tipo express
+const fs = require("fs");
 
+const home = fs.readFileSync("./index.html")
+const about = fs.readFileSync("./about.html")
+//pedir y responder
+server.get("/",getHomePag); //no le ponemos parentesis porque esto lo
+// hace express
+
+server.get("/about",(req, res)=>{
+    res.write(about) //respodemos con el archivo 
+});
+
+function getHomePag(req, res)
+{
+    return res.write(home);
+}
 server.listen(8080, ()=>{
     console.log("El server is running" + 8080);
 });
 
+
+//HTTPS methods
+
+/*
+-GET : lectura 
+-POST:  crear  un nuevo recurso
+-PUT:  reemplazr un recurso existente
+-PATCH: actualizar una propiedad de un recurso
+-DELETE: remover un recurso
+*/
